4d29c208313e6228cdff988b2d82e4392380e64d
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-11 19:49:43 -0400

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,10 @@
 const { model, Schema } = require("mongoose");
 
 module.exports = model("users", new Schema({
-    name: {},
-    email: {},
-    password: {},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    region: {type: String, required: true},
+    spotifyID: {type: String, required: false},
+    friends: [Schema.Types.ObjectId],
 }));
