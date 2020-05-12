c46e8a01dd4d3d2abefdfff4f13f637d95a32ff7
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-12 16:36:10 -0400

    added routes to run.js
    also:
    - fixed findByRouteID
    added to run controller:
    - run_update
    - run_details
    - run_delete

---
+++ routes/runs.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const auth = require("../middleware/token");
+const validation = require("../middleware/validation/runs");
+const controller = require("../controllers/runs");
+
+// Create Run
+router.post('/runs', validation.create, controller.run_create);
+
+// Get Run
+router.get('/runs', auth, controller.run_findByUserID);
+
+// Get Run by Route
+router.get('/runs', auth, controller.run_findByRouteID);
+
+// Get Run Details
+router.get('/runs/:_id', auth, controller.run_details);
+
+// Update Run
+router.put('/runs/:_id', auth, controller.run_update);
+
+// Delete Run
+router.delete('/runs/:_id', auth, controller.run_delete)
+
