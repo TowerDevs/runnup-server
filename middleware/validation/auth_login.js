76b97d5d544744770f02cb2cc8778df38ec5840e
Richard Antao <richardmantao@gmail.com>
2020-05-11 21:18:32 -0400

    Fixed errors that were present from reference

--- middleware/validation/auth_login.js
+++ middleware/validation/auth_login.js
@@ -2,22 +2,19 @@ const { body, validationResult } = require("express-validator");
 
 module.exports = (req, res, next) => {
     const errors = validationResult(req);
-    const { email, password } = req.body;
 
-    body(email, "Email received an invalid input")
+    body("email", "Email received an invalid input")
         .exists().withMessage("Email is a required field")
         .isEmail().withMessage("Email must be a valid email address")
         .escape()
         .normalizeEmail();
 
-    body(password, "Password received an invalid input")
+    body("password", "Password received an invalid input")
         .exists().withMessage("Subject is a required field")
         .escape();
     
 
-    if(!errors.isEmpty()) {
-        return res.status(400).json({ message: errors.msg });
-    };
-
+    if(!errors.isEmpty()) return res.status(400).json(errors.msg);
+   
     return next();
 };
