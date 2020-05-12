d3fdb39eb40d8a034b1c1f2dae78288673e66561
Richard Antao <richardmantao@gmail.com>
2020-05-11 21:16:25 -0400

    Fixed couple typos and used ES6 specs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,35 @@
-const User = require('../models/user')
+const User = require('../models/Users')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 
-exports.user_create = function (req, res, next) {
-    let userData = req.body
+exports.user_create = (req, res, next) => {
+    let { name, email, password, region } = req.body
+    
     User.findOne({email: userData.email}, (err, user) => {
-        if (err) {
-            console.log(err)
-        } 
+        if (err) console.log(err)
+        
         else if (user) {
                 res.statusMessage = 'User already exists'
-                res.status(403).send("User already exists")
+                res.status(403).json("User already exists")
             }
         else {
-            let user = new User(
-                {
-                    name: req.body.name,
-                    email: req.body.email,
+            let user = new User({
+                    name: name,
+                    email: email,
                     password: bcrypt.hashSync(req.body.password, 10),
-                    region: req.body.region,
+                    region: region,
                     spotifyID: null,
                     friends: null,
-                }
-            );
+            });
         
             user.save(function (err, registeredUser) {
-                if (err) {
-                    return next(err);
-                } 
-                else {
-                    let payload = { subject: registeredUser._id}
-                    let token = jwt.sign(payload, 'secretKey')
-                    res.status(200).send({token})
-                }
+                if (err) return next(err);
+                 
+                let payload = { subject: registeredUser._id}
+                let token = jwt.sign(payload, 'secretKey')
                 
+                return res.status(201).json(token)
+               
             })
         }
         
@@ -41,28 +37,27 @@ exports.user_create = function (req, res, next) {
     
 }
 
-exports.user_login = function (req, res, next) {
-    let userData = req.body
-    User.findOne({email: userData.email}, (err, user) => {
-        if (err) {
-            console.log(err)
-        } 
+exports.user_login = (req, res, next) => {
+    let { email, password } = req.body
+    
+    User.findOne({ email: email }, (err, user) => {
+        if (err) console.log(err)
+        
         else {
             if (!user) {
                 res.statusMessage = 'Invalid email'
                 res.status(401).send('Invalid email')
             } 
             else { 
-                (bcrypt.compare(userData.password, user.password, function (err, result) {
+                (bcrypt.compare(password, user.password, (err, result) => {
                     if (result) {
                         let payload = {subject: user._id}
                         let token = jwt.sign(payload, 'secretKey')
-                        res.status(200).send({token})     
-                        return true
+                        
+                        return res.status(201).json(token)     
                     } else {
                         res.statusMessage = 'Incorrect Password'
-                        res.status(401).send('Incorrect password')
-                        return false
+                        return res.status(401).send('Incorrect password')
                     }
                 })) 
             } {
@@ -73,28 +68,31 @@ exports.user_login = function (req, res, next) {
     })
 }
 
-exports.user_all = function (req, res, next) {
-    User.find(function(err, user) {
-        if (err)
-            return next(err);
-        res.json(user);
+exports.user_all = (req, res, next) => {
+    User.find((err, user) => {
+        if (err) return next(err);
+        
+        return res.status(200).json(user);
     });   
 }
 
-exports.user_details = function (req, res, next) {
-    User.findById(req.user.id, function (err, user) {
+exports.user_details = (req, res, next) => {
+    const { id } = req.user;
+    
+    User.findById(id, (err, user) => {
         if (err) return next(err);
-        res.send(user);
+        
+        return res.json(user);
     })
 };
 
 exports.user_update = function (req, res, next) {
-    User.findByIdAndUpdate(req.params.id, {$set: req.body}, 
-    function (err, user) {
-        if (err) {
-            return next(err);
-        }
-        res.send('User updated.');
+    const { id } = req.user;
+    
+    User.findByIdAndUpdate(id, {$set: req.body}, (err, user) => {
+        if (err) return next(err);
+        
+        return res.status(200).json('User updated.');
     });
 };
 
