3af89cc5c522f4793f767e98fb9483be6efc4d15
Richard Antao <richardmantao@gmail.com>
2020-05-11 20:04:57 -0400

    Added morgan for dev logging

--- server.js
+++ server.js
@@ -3,27 +3,26 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
+const morgan = require("morgan");
 const cors = require("cors");
 const xss = require("xss-clean");
 
 /* --- Configuration --- */
 const app = express();
-const port = process.env.NODE_ENV || 3001;
+const port = process.env.PORT || 3001;
+const env = process.env.NODE_ENV || "development";
 
 /* --- Middleware --- */
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(express.json());
+app.use(morgan("dev"));
 
 /* --- Routes --- */
 app.use("/api/contact", require("./routes/contact"));
 app.use("/api/errors", require("./routes/errors"));
 app.use("/api/posts", require("./routes/posts"));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/build/index.html")); // change for mobile
-});
 
 /* --- Bootup --- */
 app.listen(port, () => console.log(`Server running on port ${port}`));
