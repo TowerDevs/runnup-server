7dfd2c29e8f67b2b75de4d6116ea45336df6bf62
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-12 16:11:09 -0400

    worked on validation for routes and runs
    also fixed runs model

--- models/Runs.js
+++ models/Runs.js
@@ -4,7 +4,7 @@ module.exports = model("runs", new Schema({
     runID: { type: String, required: true },
     userID: { type: String, required: true },
     day: { type: Date, required: true },
-    routeID: {type: String, required: true},
+    userID: { type: Schema.Types.ObjectId, required: true, ref: "routes" },
     avgPace: { type: Number, required: true },
     totalTime: { type: Number, required: true }
 }, {
