8bf83a48faab52e357ea67c6e248cc06a9a3671f
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-12 17:22:41 -0400

    fix

--- models/Runs.js
+++ models/Runs.js
@@ -3,7 +3,7 @@ const { model, Schema } = require("mongoose");
 module.exports = model("runs", new Schema({
     userID: { type: Schema.Types.ObjectId, required: true, ref: "users" },
     day: { type: Date, required: true },
-    userID: { type: Schema.Types.ObjectId, required: true, ref: "routes" },
+    routeID: { type: Schema.Types.ObjectId, required: true, ref: "routes" },
     avgPace: { type: Number, required: true },
     totalTime: { type: Number, required: true }
 }, {
