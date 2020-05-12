dc09a569fe9ba2a840a2ee474ded67f1c9761756
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-12 16:27:12 -0400

    added routes in routes.js
    also added details, update and delete to route controller

---
+++ routes/routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const auth = require("../middleware/token");
+const validation = require("../middleware/validation/routes");
+const controller = require("../controllers/routes");
+
+// Create Route
+router.post('/routes', validation.create, controller.route_create);
+
+// Get Routes
+router.get('/routes', auth, controller.route_findByUserID);
+
+// Get Route Details
+router.get('/routes/:_id', auth, controller.route_details);
+
+// Update Route
+router.put('/routes/:_id', auth, controller.route_update);
+
+// Delete Route
+router.delete('/routes/:_id', auth, controller.route_delete)
+
