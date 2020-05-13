5e317f2a35ca109c8efbda68fab3faa6fe5d5643
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-13 14:25:06 -0400

    added delete new user to tests

--- tests/assertions/auth.test.js
+++ tests/assertions/auth.test.js
@@ -9,12 +9,15 @@ const { register, login } = require("../data/auth.json");
 
 chai.use(chaiHttp);
 
+let auth = '';
+
+// Create New User
 describe("POST /api/v1/users", () => {
     it("It should return a 201 status with the new user's token", done => {
         let user = {
             name: "Gerald Smith",
             password: "password",
-            email: "m7@michaeldsilva.com"
+            email: "m84@michaeldsilva.com"
         }
         chai.request(server)
         .post("/api/v1/users")
@@ -27,10 +30,13 @@ describe("POST /api/v1/users", () => {
             res.body.should.be.a("string");
 
             done();
+
+            return auth = res.body;
         });
     });
 });
 
+// Try creating user with email just used
 describe("POST /api/v1/users", () => {
     it("It should return a 403 status with string saying the user alredy exists", done => {
         let user = {
@@ -51,6 +57,25 @@ describe("POST /api/v1/users", () => {
             res.body.should.equal("User already exists")
 
             done();
+        });
+    });
+});
+
+// Delete new user
+describe("DELETE /api/v1/users", () => {
+    it("It should return a 200 status with a string saying the user has been deleted", done => {
+        chai.request(server)
+        .delete("/api/v1/users")
+        .set('Authorization', auth)
+        .end((err, res) => {
+            if(err) throw err;
+
+            res.should.have.property("status", 200);
+
+            res.body.should.be.a("string");
+
+            done();
+
         });
     });
 });
