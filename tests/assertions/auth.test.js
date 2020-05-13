0794f3a46f4d3523da79a2275957fcbc5c9e7dd1
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-12 22:11:09 -0400

    added two tests to auth
    1) making a new account
    2) checking that an account with the same email cannot be created

--- tests/assertions/auth.test.js
+++ tests/assertions/auth.test.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = "testing";
+process.env.NODE_ENV = "3002";
 
 const chai = require("chai");
 const server = require("../../server");
@@ -10,27 +10,45 @@ const { register, login } = require("../data/auth.json");
 chai.use(chaiHttp);
 
 describe("POST /api/v1/users", () => {
-    it("It should return a 201 status with the new user object", done => {
+    it("It should return a 201 status with the new user's token", done => {
+        let user = {
+            name: "Gerald Smith",
+            password: "password",
+            email: "m7@michaeldsilva.com"
+        }
         chai.request(server)
-        .post("/api/users")
-        .send(register.good)
+        .post("/api/v1/users")
+        .send(user)
         .end((err, res) => {
             if(err) throw err;
 
             res.should.have.property("status", 201);
 
-            res.body.should.be.a("object");
+            res.body.should.be.a("string");
 
-            res.body.should.have.property("name");
-            res.body.should.have.property("email");
-            res.body.should.have.property("password");
+            done();
+        });
+    });
+});
 
-            res.body.name.should.be.a("object");
-            res.body.email.should.be.a("string");
-            res.body.password.should.be.a("string");
+describe("POST /api/v1/users", () => {
+    it("It should return a 403 status with string saying the user alredy exists", done => {
+        let user = {
+            name: "Gerald Smith",
+            password: "password",
+            email: "m2@michaeldsilva.com"
+        }
+        chai.request(server)
+        .post("/api/v1/users")
+        .send(user)
+        .end((err, res) => {
+            if(err) throw err;
 
-            res.body.name.first.should.be.a("string");
-            res.body.name.last.should.be.a("string");
+            res.should.have.property("status", 403);
+
+            res.body.should.be.a("string");
+
+            res.body.should.equal("User already exists")
 
             done();
         });
