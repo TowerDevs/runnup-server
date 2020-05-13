a5eef9116122865ec4f20fddee08a0abbed989f2
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-13 17:17:35 -0400

    added check for user to friend_add

--- controllers/friends.js
+++ controllers/friends.js
@@ -8,16 +8,30 @@ exports.friend_add = (req, res, next) => {
 
     const { email } = req.body;
 
-    const friend = new Friend({
-        email: email,
-        status: "pending"
-    })
-    User.findOneAndUpdate({_id: new ObjectID(req.user)}, {$push: {friends: friend}}, (err) => {
-        if (err) return next(err);
+    console.log({email})
 
-        console.log(req.body)
+    User.findOne({ email }, (err, user) => {
 
-        return res.status(200).json('Friend Added.');
+        if (err) return console.log(err)
+
+        else if (!user) return res.status(403).json("User does not exist")
+
+        else {
+
+            const friend = new Friend({
+                email: email,
+                status: "pending"
+            })
+            User.findOneAndUpdate({_id: new ObjectID(req.user)}, {$push: {friends: friend}}, (err) => {
+                if (err) return next(err);
+        
+                console.log(req.body)
+        
+                return res.status(200).json('Friend Added.');
+            });
+
+        }
+
     });
     
 }
