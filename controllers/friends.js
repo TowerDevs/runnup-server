b12bae75a950ed798fb6b54ff076d202c6fa2707
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-13 17:46:27 -0400

    added check to see if user is trying to add themselves

--- controllers/friends.js
+++ controllers/friends.js
@@ -13,20 +13,23 @@ exports.friend_add = (req, res, next) => {
     User.findOne({ email }, (err, user) => {
 
         if (err) return console.log(err)
+        else if (!user) return res.status(404).json("User does not exist")
 
-        else if (!user) return res.status(403).json("User does not exist")
+        else if (user.id == req.user) {
+            return res.status(404).json("You cannot add yourself as a friend.")
+        }
 
         else {
+
+            console.log("User ID" + user._id)
 
             const friend = new Friend({
                 email: email,
                 status: "pending"
             })
             User.findOneAndUpdate({_id: new ObjectID(req.user)}, {$push: {friends: friend}}, (err) => {
-                if (err) return next(err);
-        
-                console.log(req.body)
-        
+                if (err) return next(err);        
+                console.log(req.body)        
                 return res.status(200).json('Friend Added.');
             });
 
