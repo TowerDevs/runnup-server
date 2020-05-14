d3430628f561016c0a1de675a96633698ef4a9a2
Richard Antao <richardmantao@gmail.com>
2020-05-14 00:10:05 -0400

    Added enum constraints

--- models/Friends.js
+++ models/Friends.js
@@ -3,5 +3,8 @@ const { model, Schema } = require("mongoose");
 module.exports = model("friends", new Schema({
     friend: { type: Schema.Types.ObjectId, ref: "users" },
     email: { type: String, required: true },
-    status: { type: String, required: true }
+    status: { type: String, required: true, enum: ["Pending", "Accepted", "Rejected", "Blocked"] }
+}, {
+    timestamps: false,
+    versionKey: false
 }))
