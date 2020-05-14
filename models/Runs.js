5bb5abe26a8dc5e151a44f73850ed69839b973e6
Richard Antao <richardmantao@gmail.com>
2020-05-14 18:16:58 -0400

    Added default values to optional properties

--- models/Runs.js
+++ models/Runs.js
@@ -4,8 +4,8 @@ module.exports = model("runs", new Schema({
     user: { type: Schema.Types.ObjectId, required: true, ref: "users" },
     timestamp: { type: Date, required: true },
     route: { type: Schema.Types.ObjectId, required: true, ref: "routes" },
-    avgPace: { type: Number, required: true },
-    totalTime: { type: Number, required: true }
+    avgPace: { type: Number, default: 0 },
+    totalTime: { type: Number, default: 0 }
 }, {
     timestamps: false,
     versionKey: false
