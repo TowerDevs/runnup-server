8dffa721beedc3544df7d3e507fa9a5ff56c0455
Richard Antao <richardmantao@gmail.com>
2020-05-14 17:49:53 -0400

    Added isEmail and normalizeEmail to validate and sanitize input

--- middleware/validation/friends.js
+++ middleware/validation/friends.js
@@ -1,13 +1,15 @@
 const { body, validationResult } = require("express-validator");
 
-exports.friend_add = (req, res, next) => {
+module.exports = (req, res, next) => {
     const errors = validationResult(req);
 
     body("email", "Email received an invalid input")
         .exists().withMessage("Email is a required field")
+        .isEmail().withMessage("Email must be a valid email address")
         .trim()
-        .escape();
-        
+        .escape()
+        .normalizeEmail();
+
     if(!errors.isEmpty()) return res.status(400).json(errors.msg);
 
     return next();
