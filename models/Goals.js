a6ff653b8caec86ecfcd249cc7789c5438933ace
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-15 17:58:49 -0400

    added options for goal result in status field

--- models/Goals.js
+++ models/Goals.js
@@ -2,11 +2,12 @@ const { model, Schema } = require("mongoose");
 
 module.exports = model("goals", new Schema({
     name: { type: String, required: true },
-    goalType: {type: String, enum: ["numberOfRuns", "minutesRan", "distanceRan"]},
-    value: { type: Number},
+    goalType: {type: String, enum: ["numberOfRuns", "minutesRan", "distanceRan"], required: true},
+    target: { type: Number, required: true},
+    actual: { type: Number},
     startDate: { type: Date },
     periodOfTime: { type: Number, required: true },
-    status: { type: String, default: "Pending", enum: ["Pending", "In Progress", "Completed"] }
+    status: { type: String, default: "Pending", enum: ["Pending", "In Progress", "Succeeded", "Failed"] },
 }, {
     timestamps: true,
     versionKey: false
