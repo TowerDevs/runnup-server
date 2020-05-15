f3d7be8077d6fb53c273e4b21033d69f4fcf0b8f
michaelddsilva <michael.dsilva.343@gmail.com>
2020-05-15 17:54:26 -0400

    changed goal model to alt format

--- models/Goals.js
+++ models/Goals.js
@@ -2,12 +2,12 @@ const { model, Schema } = require("mongoose");
 
 module.exports = model("goals", new Schema({
     name: { type: String, required: true },
-    numberOfRuns: { type: Number},
-    minutesRan: { type: Number},
-    distanceRan: { type: Number},
+    goalType: {type: String, enum: ["numberOfRuns", "minutesRan", "distanceRan"]},
+    value: { type: Number},
+    startDate: { type: Date },
     periodOfTime: { type: Number, required: true },
     status: { type: String, default: "Pending", enum: ["Pending", "In Progress", "Completed"] }
 }, {
-    timestamps: false,
+    timestamps: true,
     versionKey: false
 }))
